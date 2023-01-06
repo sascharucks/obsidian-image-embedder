be5429472e2e234c0a2c33968fbee4419c05237a
Sascha Rucks <suzhi18@gmail.com>
2023-01-07 00:00:56 +0100

    creating 0.1.0

--- main.ts
+++ main.ts
@@ -1,137 +1,79 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class ImageEmbedder extends Plugin {
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	pasteHandler = (evt: ClipboardEvent, editor: Editor) => UrlIntoSelection(editor, evt);
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'img-embedder',
+			name: 'Embed external image',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const selectedText = editor.getSelection();
+				if (await validateString(selectedText)) {
+					editor.replaceSelection(`![](${selectedText})`);
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.app.workspace.on("editor-paste", this.pasteHandler);
 	}
 
 	onunload() {
-
+		this.app.workspace.off("editor-paste", this.pasteHandler);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+async function UrlIntoSelection(editor: Editor, evt: ClipboardEvent) {
+	const cb = evt.clipboardData!.getData("text");
+	
+	// Prevent paste before async processing
+	evt.preventDefault();
+
+	if (await validateString(cb)) {
+		console.log("transform");
+		editor.replaceSelection(`![](${cb})`);	
+	} else {
+		// Still paste everthing that is not an image to embed
+		editor.replaceSelection(`${cb}`);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+async function validateString(s: string) {
+	if (isValidUrl(s)) {
+		return await isImageUrl(s)
+	} else {
+		return false;
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+function isValidUrl(url: string): boolean {
+	try {	
+		new URL(url);
+		return true;
+	} catch (err) {
+		return false;
 	}
+};
 
-	display(): void {
-		const {containerEl} = this;
+/*
+If the URL fetch failes this fallback looks at the file extensions
+Image file types from here: https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+*/
+function isImageUrlFallback(url: string): boolean {
+	console.log("img fallback");
+	return (url.match(/\.(jpeg|jpg|gif|png|webp|apng|avif|jfif|pjpeg|pjp|svg|bmp|ico|cur|tif|tiff)$/) != null);
+}
 
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+// Doesn't work with CORS
+async function isImageUrl(url: string) {
+	try {
+		const response = await fetch(url, { method: 'HEAD' });
+		return response.headers.get('Content-Type')?.startsWith('image')
+	} catch {
+		// Event with a catch CORS erros will be logged to Console, see https://stackoverflow.com/questions/41515732/hide-401-console-error-in-chrome-dev-tools-getting-401-on-fetch-call
+		return isImageUrlFallback(url);
 	}
 }
